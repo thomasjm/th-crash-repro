a75b1c25f6f14b5d8fde02c5035fc2f5f5154e0d
Tom McLaughlin <tom@mac1.lan>
2024-11-12 01:28:04 -0800

    Simplify

--- flake.nix
+++ flake.nix
@@ -25,35 +25,12 @@
 
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
 
-        flake = compiler-nix-name: src: (pkgs.hixProject compiler-nix-name src [{
-          configureFlags = [
-            ''--ghc-options="-with-rtsopts=-M12G"''
-          ];
-        }]).flake {};
-
-        flakeStatic = compiler-nix-name: src: (pkgs.pkgsCross.musl64.hixProject compiler-nix-name src [{
-          packages.th-crash-repro.components.exes.th-crash-repro.dontStrip = false;
-          packages.th-crash-repro.components.exes.th-crash-repro.enableShared = false;
-          packages.th-crash-repro.components.exes.th-crash-repro.configureFlags = [
-            ''--ghc-options="-pgml g++ -optl=-fuse-ld=gold -optl-Wl,--allow-multiple-definition -optl-Wl,--whole-archive -optl-Wl,-Bstatic -optl-Wl,-Bdynamic -optl-Wl,--no-whole-archive"''
-          ];
-          packages.th-crash-repro.components.exes.th-crash-repro.libs = [];
-          packages.th-crash-repro.components.exes.th-crash-repro.build-tools = [pkgs.pkgsCross.musl64.gcc];
-        }]).flake {};
-
-        srcWithStackYaml = stackYaml: let
-          baseSrc = gitignore.lib.gitignoreSource ./.;
-        in
-          pkgs.runCommand "src-with-${stackYaml}" {} ''
-            cp -r ${baseSrc} $out
-          '';
-
-        exeAttr = "th-crash-repro:exe:th-crash-repro";
+        flake = (pkgs.hixProject "ghc966" (gitignore.lib.gitignoreSource ./.) []).flake {};
 
       in
         rec {
           packages = rec {
-            default = (flake "ghc966" (srcWithStackYaml "stack.yaml")).packages.${exeAttr};
+            default = flake.packages."th-crash-repro:exe:th-crash-repro";
           };
 
           inherit flake;
